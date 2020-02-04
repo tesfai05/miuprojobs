546d0fb0801e53b69e495e7b665df92a598a7471
BEREKET ABRAHAM <bereketmichael741@gmail.com>
2020-02-04 15:09:48 -0600

    Finalizing the project

--- server/src/index.js
+++ server/src/index.js
@@ -16,7 +16,7 @@ const app = express();
 /////////////////////////////////////////
 // CONFIG
 /////////////////////////////////////////
-app.set(SECRET_KEY, process.env.SECRET_KEY || 'MeanJobHuntSecretKey');
+app.set(SECRET_KEY, process.env.SECRET_KEY || 'MIUProJobsSecretKey');
 
 /////////////////////////////////////////
 // MIDDLEWARE
